72141efd429c61e40ddadc02da6347ae3cc5765d
Yachana Meshram <yachanameshram@gmail.com>
2024-04-20 21:25:47 +0530

    Second file created

---
+++ Addition1.copy.java
@@ -0,0 +1,77 @@
+public class Addition{
+public static void main(String arg []){
+//int number1=10;
+//int number2=20;
+//int add;
+//add = number1+number2;
+//System.out.println("Addition of Number");
+//System.out.println(add);
+
+
+
+
+
+//float number1=10.0f;
+//float number2=20;
+//float add;
+//add = number1+number2;
+//System.out.println("Addition of Numbers");
+//System.out.println(add);
+
+
+
+
+//if (true){
+//System.out.println("in IF block");
+//}
+//else{
+//System.out.println("in else block");
+//}
+
+
+
+
+//if (false){
+//System.out.println("in IF block");
+//}
+//else{
+//System.out.println("in else block");
+//}
+
+
+//int a=10;
+//if (a>5){
+//System.out.println("in IF block");
+//}
+//else{
+//System.out.println("in else block");
+//}
+
+//int a=10;
+//if(a<5 && a>10){
+//System.out.println("in IF block");
+//}
+//else{
+//System.out.println("in else block");
+//}
+
+
+
+int a=61;
+if (a<50){
+System.out.println("in of block");
+}
+else if (a>15){
+System.out.println("in else if block");
+}
+else{
+System.out.println("in else block");
+}
+System.out.println("outside IF-ELSE");
+
+
+
+
+
+}
+}
